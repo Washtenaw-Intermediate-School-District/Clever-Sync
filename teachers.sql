dd3ab14eca00e0ea5abaf41d1ad18e44cd463d5d
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-07-21 09:43:33 -0400

    Create teachers.sql
    This is the teacher file. No adjustments made to Ryan's original other than changing the LCS schools to YCS schools and commenting out the password field.

---
+++ teachers.sql
@@ -0,0 +1,20 @@
+select
+    ss.schoolid,
+    u.dcid,
+    u.teachernumber,
+    stx.reppic,
+    u.email_addr,
+    u.first_name,
+    u.middle_name,
+    u.last_name,
+    u.title,
+    u.loginid
+    --,null as password Is this necessary if we are using Google for SSO?
+    
+from schoolstaff ss
+    join users u on ss.users_dcid=u.dcid
+    join s_mi_ssf_tsdl_x stx on ss.dcid=stx.schoolstaffdcid
+
+where ss.status=1
+    and u.ptaccess=1
+    and ss.id in (select sec.teacher from sections sec where sec.termid in (select t.id from terms t where sysdate between t.firstday-20 and t.lastday+20) and sec.schoolid IN ('1925','1923','3000','9404','1153','1157','2988','1705','798','2062','1938'))
