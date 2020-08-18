3976e8cce087f252b44d4e6d0e514f1bc6338469
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-08-18 10:33:33 -0400

    aligned student id fields
    aligned the student id field from the enrollments query to the students id being provided on the students query.

--- batch file formatted/enrollments.sql
+++ batch file formatted/enrollments.sql
@@ -9,8 +9,9 @@ PROMPT School_id,Section_id,Student_id
 select
 cc.schoolid||','||
 cc.sectionid||','||
-cc.studentid
+students.dcid
 from cc
+join students on cc.studentid = students.id
 where 
     cc.schoolid in (1925,1923,3000,9404,1153,1157,2988,1705,798,2062,1938)
     and sysdate between cc.dateenrolled-40 and cc.dateleft+20;
--- enrollments.sql
+++ enrollments.sql
@@ -1,9 +1,10 @@
 select
     cc.schoolid,
     cc.sectionid,
-    cc.studentid
+    students.dcid
 
 from cc
+    join students on cc.studentid = students.id
 
 where 
     cc.schoolid in (1925,1923,3000,9404,1153,1157,2988,1705,798,2062,1938)
