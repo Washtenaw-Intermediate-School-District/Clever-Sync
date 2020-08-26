bc5bb5d5682748ba49717fc536e3bf0c086119e9
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-08-26 13:14:20 -0400

    Update teachers.sql
    This is to increase the number of teachers being pushed into Clever until all sections are scheduled.

--- teachers.sql
+++ teachers.sql
@@ -17,4 +17,4 @@ from schoolstaff ss
 
 where ss.status=1
     and u.ptaccess=1
-    and ss.id in (select sec.teacher from sections sec where sec.termid in (select t.id from terms t where sysdate between t.firstday-20 and t.lastday+20) and sec.schoolid IN ('1925','1923','3000','9404','1153','1157','2988','1705','798','2062','1938'))
+    --and ss.id in (select sec.teacher from sections sec where sec.termid in (select t.id from terms t where sysdate between t.firstday-20 and t.lastday+20) and sec.schoolid IN ('1925','1923','3000','9404','1153','1157','2988','1705','798','2062','1938'))
